d70e7d77a7285147201013d300da1aa56165b10b
Calvin Nichols <calvindn@gmail.com>
2013-06-26 21:54:38 -0400

    cleaned up alert functions

--- public/js/views/project/add.js
+++ public/js/views/project/add.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module){
+define(function(require, exports, module) {
     var $               = require('jquery'),
         _               = require('underscore'),
         Backbone        = require('Backbone'),
@@ -10,11 +10,12 @@ define(function(require, exports, module){
         Projects        = require('models/ProjectCollection');
 
 module.exports = Backbone.View.extend({
-    initialize: function (){
+
+    initialize: function () {
         this.resources = 0; // number of resources added to project
     },
 
-    render: function (){
+    render: function () {
         this.$el.html(_.template(ProjectTemplate));
         return this;
     },
@@ -27,13 +28,7 @@ module.exports = Backbone.View.extend({
         "click .remove-rsc"   : "removeResourceHtml"
     },
 
-    // VALIDATION / ALERTS IS A FUCKING MESS IN HERE
-    //
-    // to handle remove resource use e.target to remove class
-    // dont decrement on removal and check before pulling the values
-    // on submission of form
-
-    appendResourceHtml: function(e){
+    appendResourceHtml: function(e) {
         if (typeof e != "undefined")
             e.preventDefault();
         // COULDDO: add a seperate view for resources
@@ -41,7 +36,7 @@ module.exports = Backbone.View.extend({
         this.$(".resourceList-append").append(_.template(newResource, {number: this.resources}));
     },
 
-    clearResourceHtml: function(){
+    clearResourceHtml: function() {
         var len = this.resources;
         for (var i=1; i < len+1; i++){
             this.$(".resource-"+i).remove();
@@ -49,26 +44,19 @@ module.exports = Backbone.View.extend({
         }
     },
 
-    removeResourceHtml: function(e){
-        //if (typeof e != "undefined")
-            //e.preventDefault();
+    removeResourceHtml: function(e) {
         $("."+e.target.classList[4]).remove();
-        //console.log(e.target.classList[4]);
-        //console.log(e);
     },
 
-    validateForm: function (){
-        // this destroy prevents a bug that allows you to skip validation on resources
-        // that have been added a first (unsuccessful) submission attempt (and validation)
-        // prevents submitting blank resources
+    validateForm: function () {
         this.$( '#addProjectForm' ).parsley( 'destroy' );
         if (this.$('#addProjectForm').parsley( 'validate' ))
             return this.saveProject();
 
-        return this.$('.alert').animate({opacity:0});
+        this.toggleAlert(0);
     },
 
-    saveProject: function (){
+    saveProject: function () {
         var self = this;
 
         var resourceList = [];
@@ -82,47 +70,59 @@ module.exports = Backbone.View.extend({
             resources   : resourceList
         }).save(null, {
             wait: true,
-            success: function(response, model){
-                if (self.$('.alert').css('opacity') == 1)
-                    self.$('.alert').css({opacity:0});
+            success: function(response, model) {
+                self.toggleAlert(0);
 
-                setTimeout(function(){
-                    self.$( '#addProjectForm' ).parsley( 'destroy' );
-                    self.$("#addProjectForm")[0].reset();
-                    self.clearResourceHtml();
-                },1600);
+                setTimeout(function() {
+                    self.clearForm();
+                },1400);
                 self.updateAlert('alert-success');
-                // SHOULDDO: Add self code to updateAlert
-                self.$('.alert').html('<a class="close">✕</a><strong>Success!</strong> Project added to database.');
-                self.$('.alert').animate({opacity:1});
             },
-            error: function(response){
-                // MUSTDO: Investigate if this alert is being displayed correctly
-                console.log(response);
-                if (self.$('.alert').css('opacity') == 1)
-                    self.$('.alert').css({opacity:0});
+            error: function(response) {
+                self.toggleAlert(0);
                 self.updateAlert('alert-error');
-                self.$('.alert').html('<a class="close">✕</a><strong>Error!</strong> Something went wrong.');
-                self.$('.alert').animate({opacity:1});
             }
         });
     },
 
-    updateAlert: function(newClass){
-        this.$('.alert').removeClass('alert-success').removeClass('alert-error').removeClass('alert-warn').addClass(newClass);
+    updateAlert: function(newClass) {
+        this.$('.alert')
+        .removeClass('alert-success')
+        .removeClass('alert-error')
+        .addClass(newClass);
+
+        this.displayAlert(newClass);
     },
 
-    closeAlert: function (e){
+    displayAlert: function(newClass) {
+        if (newClass === 'alert-success')
+            this.$('.alert').html('<a class="close">✕</a><strong>Success!</strong> Project added to database.');
+        else
+            this.$('.alert').html('<a class="close">✕</a><strong>Error!</strong> Something went wrong.');
+
+        this.toggleAlert(1);
+    },
+
+    toggleAlert: function(alertOpacity) {
+        this.$('.alert').animate({opacity: alertOpacity});
+    },
+
+    closeAlert: function (e) {
         this.$(e.target).closest('.alert').animate({opacity:0});
     },
 
-    clearForm: function (){
+    clearForm: function (e) {
         this.$( '#addProjectForm' ).parsley( 'destroy' );
-        this.$('.alert').animate({opacity:0});
+        $("#addProjectForm")[0].reset();
         this.clearResourceHtml();
+
+        if (typeof e === "undefined") // if not button press dont clear
+            this.toggleAlert(1);
+        else
+            this.toggleAlert(0);
     },
 
-    gatherResources: function(resourceList){
+    gatherResources: function(resourceList) {
         for (var i=1; i < this.resources+1; i++){
             if ($(".resource-"+i)[0]){
                 var item = new Resource({
@@ -135,7 +135,7 @@ module.exports = Backbone.View.extend({
         }
     },
 
-    remove: function(){
+    remove: function() {
         this.undelegateEvents();
         this.$el.empty();
         this.stopListening();
