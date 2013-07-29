68bcefcdd6d43f73a0ba630d97005b5ea8c6c54d
Calvin Nichols <calvindn@gmail.com>
2013-07-28 22:51:19 -0400

    removed settings files

--- settings/mail.js
+++
@@ -1,9 +0,0 @@
-var mail = {};
-
-mail.service   = "Gmail";
-mail.user      = "calvindn@gmail.com";
-mail.password  = "sL0077Jx&xgxIuBi%812";
-mail.sender    = "calvin@calvindn.com";
-mail.receiver  = "calvin@calvindn.com";
-
-module.exports = mail;
